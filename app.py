2a686b5d11aedbe950dddd7ec55d1633de359084
albertmartinmoreno <albertmartinmoreno@icloud.com>
2024-03-15 19:13:03 +0000

    Upload changes

--- app.py
+++ app.py
@@ -2,19 +2,22 @@ from quart import Quart
 from flask_smorest import Api
 import os
 from dotenv import load_dotenv
-from quart_cors import cors
+from flask_cors import CORS
+from database import database
 
 load_dotenv()
 
 app: Quart = Quart(__name__)
 
-app = cors(app, allow_origin='*')
-
 app.config['API_TITLE'] = os.getenv('API_TITLE')
 app.config['API_VERSION'] = os.getenv('API_VERSION')
 app.config['OPENAPI_VERSION'] = os.getenv('OPENAPI_VERSION')
 
+database.init_app(app)
+
 api: Api = Api(app)
+
+CORS(app, resources={r'*': {'origins': '*'}})
 
 if __name__ == '__main__':
     app.run(debug=True)
