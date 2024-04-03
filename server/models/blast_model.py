61e4ad7fea3489076c0a5e8dce9499212166cb98
almamodev <almamodev@gmail.com>
2024-04-03 21:08:33 +0200

    blast model almost done

--- server/models/blast_model.py
+++ server/models/blast_model.py
@@ -2,86 +2,170 @@ from marshmallow import Schema, validate, validates, ValidationError
 from marshmallow.validate import OneOf, Regexp
 from marshmallow.fields import Raw, String
 from werkzeug.datastructures import FileStorage
-from typing import Any, TextIO, Optional
+from typing import Optional
 from Bio import Entrez
 from Bio.Entrez import efetch
 from urllib.error import HTTPError
 from re import match
+from os.path import exists
 
 
 class Blast:
-    def __init__(self, fasta: str, database: str, algorithm: str, file: Optional[FileStorage] = None, genbank: Optional[str] = None) -> None:
+    def __init__(
+        self: Blast,
+        sequence: str,
+        database: str,
+        algorithm: str,
+        file: Optional[FileStorage] = None,
+        genbank: Optional[str] = None
+    ) -> None:
         """
-
-        :param fasta:
-        :param database:
-        :param algorithm:
-        :param file:
-        :param genbank:
+        :param sequence: FASTA sequence to compare
+        :param database: NCBI database to search
+        :param algorithm: BLAST algorithm to calculate statistical significance
+        :param file: FASTA file sequence to compare
+        :param genbank: GenBank FASTA sequence to compare
         """
-        self.fasta = fasta
-        self.file = file
-        self.genbank = genbank
-        self.database = database
-        self.algorithm = algorithm
+        self.sequence: str = sequence
+        self.database: str = database
+        self.algorithm: str = algorithm
+        self.file: FileStorage | None = file
+        self.genbank: str | None = genbank
 
 
 class BlastModel(Schema):
-    fasta: String = String(validate=Regexp(regex=r'', error=''), required=True)
+    sequence: String = String(
+        validate=Regexp(
+            # optional character, one or more non-whitespace characters, and optionally any character sequence
+            regex=r'^>?(\S+)\s*(.+)?$',
+            error=''
+        ),
+        required=True
+    )
 
-    file: Raw = Raw(metadata={'type': 'string', 'format': 'binary'})
+    file: Raw = Raw(
+        metadata={
+            'type': 'string',
+            'format': 'binary'
+        }
+    )
 
-    genbank: String = String(validate=Regexp(regex=r'[A-Z]{2}_?\d+(\.\d+)?', error=''))
+    genbank: String = String(
+        validate=Regexp(
+            # two uppercase letters, optional underscore, one or more digits, optionally a decimal point and one or more digits
+            regex=r'[A-Z]{2}_?\d+(\.\d+)?',
+            error=''
+        )
+    )
 
-    database: String = String(validate=OneOf(choices=[
-        'nr',
-        'nt',
-        'refseq_select_rna',
-        'refseq_select_prot'
-        'refseq_rna',
-        'env_nr',
-        'tsa_nt',
-        'patnt',
-        'pdbnt',
-        'refseq_protein',
-        'landmark',
-        'swissprot',
-        'pataa',
-        'pdbaa',
-        'env_nr',
-        'tsa_nr',
-        'Betacoronavirus'
-    ], error=''), required=True)
+    database: String = String(
+        validate=OneOf(
+            choices=[
+                'nr',
+                'nt',
+                'refseq_select_rna',
+                'refseq_select_prot'
+                'refseq_rna',
+                'env_nr',
+                'tsa_nt',
+                'patnt',
+                'pdbnt',
+                'refseq_protein',
+                'landmark',
+                'swissprot',
+                'pataa',
+                'pdbaa',
+                'env_nr',
+                'tsa_nr',
+                'Betacoronavirus'
+            ],
+            error=''
+        ),
+        required=True
+    )
 
-    algorithm: String = String(validate=OneOf(choices=[
-        'blastn',
-        'blastp',
-        'blastx',
-        'tblastn',
-        'tblastx'
-    ], error=''), required=True)
+    algorithm: String = String(
+        validate=OneOf(
+            choices=[
+                'blastn',
+                'blastp',
+                'blastx',
+                'tblastn',
+                'tblastx'
+            ],
+            error=''
+        ),
+        required=True
+    )
 
     @validates('genbank')
-    def validates_genbank(self: BlastModel, genbank: str) -> None:
+    def validates_genbank(
+        self: BlastModel,
+        genbank: str
+    ) -> None:
         """
+        Searchs GenBank FASTA file and saves it
 
-        :param genbank:
-        :return:
+        :param genbank: GenBank to search
+        :return: None
         """
         try:
             Entrez.email: str = 'info@biocloudlabs.es'
-            fasta: TextIO = efetch(db='nuccore', id=genbank, rettype='fasta')
-            with open(file=f'files/{genbank}.fasta', mode='w') as file:
-                file.write(__s=fasta.read())
+            with open(
+                file='files/sequence.fasta',
+                mode='w'
+            ) as file:
+                file.write(__s=efetch(
+                    db='nuccore',
+                    id=genbank,
+                    rettype='fasta'
+                ).read())
         except HTTPError:
             raise ValidationError(message='')
 
-    @validates('fasta')
-    def validates_fasta(self: BlastModel, fasta: str) -> None:
+    @validates('sequence')
+    def validates_sequence(
+        self: BlastModel,
+        sequence: str
+    ) -> None:
         """
-
-        :param fasta:
+        :param sequence:
         :return:
         """
-        with open(file=f"files/{match(pattern=r'[A-Z]{2}_?\d+(\.\d+)?', string=fasta).group()}.fasta") as file:
-            file.write(__s=fasta)
+        with open(
+            file='files/sequence.fasta',
+            mode='w'
+        ) as file:
+            file.write(__s=sequence)
+
+
+    @validates('file')
+    def validates_file(
+        self: BlastModel,
+        file: FileStorage
+    ) -> None:
+        """
+        :param file: FASTA file
+        :return:
+        """
+        if not file.filename.endswith(__suffix=(
+            '.fasta',
+            '.fas',
+            '.fa',
+            '.fna',
+            '.ffn',
+            '.faa',
+            'mpfa',
+            'frn'
+        )):
+            raise ValidationError(message='')
+
+        if not match(
+            pattern=r'^>(\S+)\s+(.+)$',
+            string=file.stream.read().decode()
+        ):
+            raise ValidationError(message='')
+
+        file.save('files/sequence.fasta')
+
+
