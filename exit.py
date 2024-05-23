02e8e7ee6e69017a95138856b3b7ccdbcf4515e4
albertmartinmoreno <160793560+albertmartinmoreno@users.noreply.github.com>
2024-05-23 17:22:34 +0200

    Update exit.py

--- exit.py
+++ exit.py
@@ -1,8 +1,12 @@
+# Define a custom exception class named Exit
 class Exit(Exception):
+    # Initialize the exception with a status code
     def __init__(self, status: int) -> None:
-        self.status = status
+        self.status = status  # Store the status code in the instance
 
+    # Define the string representation of the exception
     def __str__(self) -> str:
+        # Dictionary mapping status codes to error messages
         MESSAGE = {
             1: 'Error in query sequence(s) or BLAST options',
             2: 'Error in BLAST database',
@@ -12,4 +16,5 @@ class Exit(Exception):
             6: 'Error creating output files'
         }
         
+        # Return the error message corresponding to the status code
         return MESSAGE[self.status]
