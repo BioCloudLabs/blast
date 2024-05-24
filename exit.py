aece224a193bf0c907eccd1911e88886af84bbc5
albertmartinmoreno <160793560+albertmartinmoreno@users.noreply.github.com>
2024-05-24 15:19:54 +0200

    Update exit.py

--- exit.py
+++ exit.py
@@ -11,7 +11,7 @@ class Exit(Exception):
             1: 'Error in query sequence(s) or BLAST options',
             2: 'Error in BLAST database',
             3: 'Error in BLAST engine',
-            4: 'Out of memory',
+            137: 'Out of memory',
             5: 'Network error connecting to NCBI to fetch sequence data',
             6: 'Error creating output files'
         }
